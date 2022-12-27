f1e704afd0e0f550103c09b3fb6d42e01be21dd7
davidshiao55 <davidshiao55@gmail.com>
2022-12-27 21:27:50 +0800

    task_context

--- hw3/os_2022_hw3-davidshiao55/src/task.c
+++ hw3/os_2022_hw3-davidshiao55/src/task.c
@@ -19,7 +19,6 @@ int algorithm;
 int taskCount = 0;
 bool sim = false;
 Task *curr_task = NULL;
-contextNode *contextList = NULL;
 ucontext_t ctx_main;
 void (*funcs[9])() = {task1, task2, task3, task4, task5, task6, task7, task8, task9};
 
@@ -34,14 +33,10 @@ void my_alarm_handler(int sig)
             rr_count++;
             if (rr_count == 3)
             {
-                printf("TIMEUP\n");
                 rr_count = 0;
                 curr_task->task_state = READY;
                 enQueue(ready_queue, curr_task);
-                contextNode *timeupContext = findContext(curr_task);
-                if (!timeupContext)
-                    fprintf(stderr, "context switch error\n");
-                swapcontext(&timeupContext->ctx, &ctx_main);
+                swapcontext(&(curr_task->task_context->ctx), &ctx_main);
             }
         }
     }
@@ -60,7 +55,7 @@ void my_alarm_handler(int sig)
         wNode->t->sleep_time--;
         if (wNode->t->sleep_time <= 0)
         {
-            printf("waking up task %s\n", wNode->t->task_name);
+            // printf("waking up task %s\n", wNode->t->task_name);
             wNode->t->task_state = READY;
             enQueue(ready_queue, wNode->t);
             deleteQueue(waiting_queue, wNode->t);
@@ -99,17 +94,80 @@ void task_manager_destroy()
     detroyQueue(task_queue, true);
 }
 
+void startSimulation()
+{
+    pthread_t tid;
+    sim = true;
+    pthread_create(&tid, NULL, simulation, NULL);
+}
+
+void *simulation(void *vargp)
+{
+    /* set timer */
+    struct itimerval tv;
+    tv.it_interval.tv_usec = 10000;
+    tv.it_interval.tv_sec = 0;
+    tv.it_value.tv_usec = 10000;
+    tv.it_value.tv_sec = 0;
+
+    if (setitimer(ITIMER_REAL, &tv, NULL) < 0)
+    {
+        perror("settimer error.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGALRM, my_alarm_handler);
+
+    while (sim)
+    {
+        if (ready_queue->front)
+        {
+            curr_task = ready_queue->front->t;
+            curr_task->task_state = RUNNING;
+            deQueue(ready_queue);
+            printf("Task %s is running.\n", curr_task->task_name);
+            swapcontext(&ctx_main, &(curr_task->task_context->ctx));
+            /* if task is terminated than release context & stack : seg fault*/
+            // if (curr_task->task_state == TERMINATED)
+            //     deleteContext(curr_task);
+            curr_task = NULL;
+        }
+        else
+        {
+            if (!waiting_queue->front)
+            {
+                break;
+            }
+            /* ready q empty but waiting queue isn't */
+            else
+            {
+                printf("CPU idle\n");
+                while (waiting_queue->front)
+                {
+                }
+            }
+        }
+    }
+    printf("SIMULATION END\n");
+
+    // clear timer
+    tv.it_value.tv_usec = 0;
+    tv.it_value.tv_sec = 0;
+    if (setitimer(ITIMER_REAL, &tv, NULL) < 0)
+    {
+        perror("settimer error.\n");
+        exit(EXIT_FAILURE);
+    }
+    pthread_exit(NULL);
+}
+
 void task_sleep(int ms)
 {
     printf("Task %s goes to sleep.\n", curr_task->task_name);
     curr_task->task_state = WAITING;
     curr_task->sleep_time = ms / 10;
     enQueue(waiting_queue, curr_task);
-
-    contextNode *sleepContext = findContext(curr_task);
-    if (!sleepContext)
-        fprintf(stderr, "context switch error\n");
-    swapcontext(&sleepContext->ctx, &ctx_main);
+    swapcontext(&(curr_task->task_context->ctx), &ctx_main);
 }
 
 void task_exit()
@@ -117,11 +175,7 @@ void task_exit()
     printf("Task %s is killed.\n", curr_task->task_name);
     curr_task->task_state = TERMINATED;
     curr_task->turnaround = curr_task->running_time + curr_task->waiting_time;
-
-    contextNode *finsihContext = findContext(curr_task);
-    if (!finsihContext)
-        fprintf(stderr, "context switch error\n");
-    swapcontext(&finsihContext->ctx, &ctx_main);
+    swapcontext(&(curr_task->task_context->ctx), &ctx_main);
 }
 
 void addTask(char *taskName, char *functionName, int priority)
@@ -204,142 +258,23 @@ void taskInfo(Task *t)
     printf("\t%8d\n", t->priority);
 }
 
-void startSimulation()
+Task_context *newContext(int f)
 {
-    pthread_t tid;
-    sim = true;
-    pthread_create(&tid, NULL, simulation, NULL);
-}
-
-void *simulation(void *vargp)
-{
-    /* set timer */
-    struct itimerval tv;
-    tv.it_interval.tv_usec = 10000;
-    tv.it_interval.tv_sec = 0;
-    tv.it_value.tv_usec = 10000;
-    tv.it_value.tv_sec = 0;
-
-    if (setitimer(ITIMER_REAL, &tv, NULL) < 0)
-    {
-        perror("settimer error.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    signal(SIGALRM, my_alarm_handler);
-
-    while (sim)
-    {
-        if (ready_queue->front)
-        {
-            curr_task = ready_queue->front->t;
-            curr_task->task_state = RUNNING;
-            deQueue(ready_queue);
-            printf("Task %s is running.\n", curr_task->task_name);
-            contextNode *readyContext = findContext(curr_task);
-            if (!readyContext)
-            {
-                addContext(curr_task);
-                readyContext = findContext(curr_task);
-            }
-            swapcontext(&ctx_main, &readyContext->ctx);
-            /* if task is terminated than release context & stack */
-            // if (curr_task->task_state == TERMINATED)
-            //     deleteContext(curr_task);
-            curr_task = NULL;
-        }
-        else
-        {
-            if (!waiting_queue->front)
-            {
-                break;
-            }
-            /* ready q empty but waiting queue isn't */
-            else
-            {
-                printf("CPU idle\n");
-                while (waiting_queue->front)
-                {
-                }
-            }
-        }
-    }
-    printf("SIMULATION END\n");
-
-    // clear timer
-    tv.it_value.tv_usec = 0;
-    tv.it_value.tv_sec = 0;
-    if (setitimer(ITIMER_REAL, &tv, NULL) < 0)
-    {
-        perror("settimer error.\n");
-        exit(EXIT_FAILURE);
-    }
-    pthread_exit(NULL);
-}
-
-contextNode *newContext(Task *t)
-{
-    contextNode *n = (contextNode *)malloc(sizeof(contextNode));
-    n->tid = t->tid;
-    n->next = NULL;
-    getcontext(&n->ctx);
-    n->ctx.uc_stack.ss_sp = n->stack;
-    n->ctx.uc_stack.ss_size = sizeof(n->stack);
-    n->ctx.uc_stack.ss_flags = 0;
-    n->ctx.uc_link = &ctx_main;
-    makecontext(&n->ctx, funcs[t->function - 1], 0);
-    return n;
-}
-
-void addContext(Task *t)
-{
-    contextNode *n = newContext(t);
-    if (!contextList)
-    {
-        contextList = n;
-    }
-    else
-    {
-        n->next = contextList;
-        contextList = n;
-    }
+    Task_context *tc = (Task_context *)malloc(sizeof(Task_context));
+    getcontext(&tc->ctx);
+    tc->ctx.uc_stack.ss_sp = tc->stack;
+    tc->ctx.uc_stack.ss_size = sizeof(tc->stack);
+    tc->ctx.uc_stack.ss_flags = 0;
+    tc->ctx.uc_link = &ctx_main;
+    makecontext(&tc->ctx, funcs[f - 1], 0);
+    return tc;
 }
 
 void deleteContext(Task *t)
 {
-    printf("DELETING CONTEXT\n");
-    contextNode *pre = NULL;
-    contextNode *tmp = contextList;
-    while (tmp)
-    {
-        if (tmp->tid == t->tid)
-        {
-            if (tmp == contextList)
-                contextList = tmp->next;
-            else
-                pre->next = tmp->next;
-            free(tmp);
-            break;
-        }
-        pre = tmp;
-        tmp = tmp->next;
-    }
-}
-
-contextNode *findContext(Task *t)
-{
-    printf("finding context\n");
-    contextNode *n = contextList;
-    while (n)
-    {
-        if (n->tid == t->tid)
-        {
-            return n;
-        }
-        n = n->next;
-    }
-    printf("context found\n");
-    return NULL;
+    printf("DELETE task context %s\n", t->task_name);
+    free(t->task_context);
+    t->task_context = NULL;
 }
 
 Task *newTask(char *taskName, char *functionName, int priority)
@@ -355,6 +290,7 @@ Task *newTask(char *taskName, char *functionName, int priority)
     t->sleep_time = 0;
     t->turnaround = 0;
     t->task_state = READY;
+    t->task_context = newContext(t->function);
     for (int i = 0; i < MAX_RESOURCE; i++)
         t->resources[i] = false;
     return t;
@@ -362,6 +298,8 @@ Task *newTask(char *taskName, char *functionName, int priority)
 
 void destroyTask(Task *t)
 {
+    if (t->task_context)
+        free(t->task_context);
     free(t->task_name);
     free(t);
 }
